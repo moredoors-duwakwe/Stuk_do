a8a48b50a1eed00f531ca036f88a2c1fbf805fc9
moredoors-duwakwe <jessekelvin1@gmail.com>
2016-09-14 01:15:51 -0400

    Applied new design to old task logic

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :tasks do
+  	member do 
+  		put :change
+  	end
+  end
+  devise_for :users
+  root 'pages#home'
+
+  get 'about' => "pages#about"
+
+  get 'test' => "pages#test"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
